15491f073a18094233c2da7aa2ebe491d198dc3e
TaleaX <sophie.dehne01@gmail.com>
2022-09-24 18:20:17 +0200

    added cost matching

--- get_plane_data.py
+++ get_plane_data.py
@@ -3,7 +3,18 @@ import requests
 req = requests.get('https://api.flightapi.io/onewaytrip/632ddcfccaa6ed253fcf9e6e/BER/CDG/2022-10-03/2/0/1/Economy/EUR')
 
 data = req.json()
-for flight in data['legs']:
-    print(u"day:[%s]\tduration:[%s]\tairline:[%s]\tstopOverDur[%s]" % (flight['departureDateTime'], flight['duration'], flight['airlineCodes'], flight['stopoverDurationMinutes']) )
 
-print("lol")
+def get_fare_flight(flight_id):
+    flight_id = flight_id.rstrip(":0").lstrip("BER-CDG:")
+    for flight in data['fares']:
+        tripId = flight['tripId'].split(':')[1].replace("-", ":")
+        if (tripId == flight_id):
+            return (flight)
+    return (False)
+
+for flight in data['legs']:
+    fare_flight = get_fare_flight(flight['id'])
+    if (not fare_flight):
+        #print(u"id:[%s]")
+        continue
+    print(u"id:[%s]\tday:[%s]\tduration:[%s]\tairline:[%s]\tstopOverDur[%s]\tcosts[%s]\tfare_id:[%s]" % (flight['id'],flight['departureDateTime'], flight['duration'], flight['airlineCodes'], flight['stopoverDurationMinutes'], fare_flight['price']['totalAmount'], fare_flight['tripId']))

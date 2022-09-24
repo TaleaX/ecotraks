8c2ca5a029d6893c0e07530ff1fb1e9f6bd682de
TaleaX <sophie.dehne01@gmail.com>
2022-09-24 18:43:22 +0200

    changed paris to munich

--- get_plane_data.py
+++ get_plane_data.py
@@ -1,11 +1,11 @@
 import requests
 
-req = requests.get('https://api.flightapi.io/onewaytrip/632ddcfccaa6ed253fcf9e6e/BER/CDG/2022-10-03/2/0/1/Economy/EUR')
+req = requests.get('https://api.flightapi.io/onewaytrip/632ddcfccaa6ed253fcf9e6e/BER/MUC/2022-10-03/2/0/1/Economy/EUR')
 
 data = req.json()
 
 def get_fare_flight(flight_id):
-    flight_id = flight_id.rstrip(":0").lstrip("BER-CDG:")
+    flight_id = flight_id.rstrip(":0").lstrip("BER-MUC:")
     for flight in data['fares']:
         tripId = flight['tripId'].split(':')[1].replace("-", ":")
         if (tripId == flight_id):
@@ -15,6 +15,5 @@ def get_fare_flight(flight_id):
 for flight in data['legs']:
     fare_flight = get_fare_flight(flight['id'])
     if (not fare_flight):
-        #print(u"id:[%s]")
         continue
     print(u"id:[%s]\tday:[%s]\tduration:[%s]\tairline:[%s]\tstopOverDur[%s]\tcosts[%s]\tfare_id:[%s]" % (flight['id'],flight['departureDateTime'], flight['duration'], flight['airlineCodes'], flight['stopoverDurationMinutes'], fare_flight['price']['totalAmount'], fare_flight['tripId']))
